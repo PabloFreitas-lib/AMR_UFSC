3fd470b3bfcbf5d98e006d7f76f534f9d4e476d9
freitas <pablo.santos1999@gmail.com>
2019-08-12 15:19:00 -0300

    Enconders update

--- encoders_working/encoders_working.ino
+++ encoders_working/encoders_working.ino
@@ -1,4 +1,5 @@
-/*
+
+ /*
 #  Chefbot_ROS_Interface.ino
 #
 #  Copyright 2015 Lentin Joseph <qboticslabs@gmail.com>
@@ -20,7 +21,6 @@
 #
 #  Some of the portion is adapted from I2C lib example code for MPU 6050
 */
-
 
 //MPU 6050 Interfacing libraries
 
@@ -81,7 +81,7 @@ float ypr[3];
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
 void dmpDataReady() {
-   mpuInterrupt = true;
+    mpuInterrupt = true;
 }
 
 
@@ -98,15 +98,15 @@ void dmpDataReady() {
 
 
 //PWM 1 pin number
-#define PWM_1 PC_5
+#define PWM_1 PA_4
 
 
 //Right Motor
 
-#define B_1 PA_4
+#define B_1 PA_5
 
 //PWM 2 pin number
-#define PWM_2 PC_6
+#define PWM_2 PA_6
 
 //HIGH ativa a ponte H
 //LOW desativa
@@ -144,14 +144,14 @@ float motor_right_speed = 0;
 // Left encoder 5V( preto GND - Vermelho VCC)
 
 #define Left_Encoder_PinA PD_7 // branco
-#define Left_Encoder_PinB PF_4 // verde
+#define Left_Encoder_PinB PD_6 // verde
 
 volatile long Left_Encoder_Ticks = 0;
 
 // Right encoder 5V( preto GND - Vermelho VCC)
 
-#define Right_Encoder_PinA PC_7 // branco
-#define Right_Encoder_PinB PD_6 // verde
+#define Right_Encoder_PinA PE_2 // branco
+#define Right_Encoder_PinB PE_3 // verde
 
 volatile long Right_Encoder_Ticks = 0;
 
@@ -160,35 +160,40 @@ volatile long Right_Encoder_Ticks = 0;
 
 void SetupEncoders()
 {
-  pinMode(Left_Encoder_PinA,INPUT_PULLUP);
-  pinMode(Left_Encoder_PinB,INPUT_PULLUP);
+   pinMode(Left_Encoder_PinA,INPUT_PULLUP);
+   pinMode(Left_Encoder_PinB,INPUT_PULLUP);
 
-  pinMode(Right_Encoder_PinA,INPUT_PULLUP);
-  pinMode(Right_Encoder_PinB,INPUT_PULLUP);
+   pinMode(Right_Encoder_PinA,INPUT_PULLUP);
+   pinMode(Right_Encoder_PinB,INPUT_PULLUP);
 
-  attachInterrupt(Left_Encoder_PinA, do_Left_Encoder, RISING);
-  attachInterrupt(Right_Encoder_PinA, do_Right_Encoder, RISING);
+   attachInterrupt(Left_Encoder_PinA, do_Left_Encoder, RISING);
+   attachInterrupt(Right_Encoder_PinA, do_Right_Encoder, RISING);
 }
-int a=0,b=0;
+ int a=0,b=0;
 
 void setup()
 {
 
- //Init Serial port with 115200 baud rate
- Serial.begin(115200);
+  //Init Serial port with 115200 baud rate
+  Serial.begin(115200);
 
- //Setup Encoders
- SetupEncoders();
- //Setup Motors
- SetupMotors();
- //Setup Ultrasonic
- //SetupUltrasonic();
- //Setup MPU 6050
- //Setup_MPU6050();
- //Setup Reset pins
- SetupReset();
- //Set up Messenger
- Messenger_Handler.attach(OnMssageCompleted);
+  //Setup Encoders
+  SetupEncoders();
+
+  //Setup Motors
+  SetupMotors();
+
+  //Setup Ultrasonic
+  //SetupUltrasonic();
+
+  //Setup MPU 6050
+  //Setup_MPU6050();
+
+  //Setup Reset pins
+  SetupReset();
+
+  //Set up Messenger
+  Messenger_Handler.attach(OnMssageCompleted);
 
 
 }
@@ -197,8 +202,8 @@ void setup()
 //Setup UltrasonicsSensor() function
 void SetupUltrasonic()
 {
-pinMode(Trig, OUTPUT);
-pinMode(echo, INPUT);
+ pinMode(Trig, OUTPUT);
+ pinMode(echo, INPUT);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -207,17 +212,17 @@ pinMode(echo, INPUT);
 void SetupMotors()
 {
 
-//Left motor
-pinMode(A_1,OUTPUT);
-pinMode(PWM_1,OUTPUT);
+ //Left motor
+ pinMode(A_1,OUTPUT);
+ pinMode(PWM_1,OUTPUT);
 
-//Right Motor
-pinMode(B_1,OUTPUT);
-pinMode(PWM_2,OUTPUT);
+ //Right Motor
+ pinMode(B_1,OUTPUT);
+ pinMode(PWM_2,OUTPUT);
 
-//PIN RESET MOTOR
-//pinMode(STOP_MOTOR_R,OUTPUT);
-//pinMode(STOP_MOTOR_L,OUTPUT);
+ //PIN RESET MOTOR
+ //pinMode(STOP_MOTOR_R,OUTPUT);
+ //pinMode(STOP_MOTOR_L,OUTPUT);
 
 }
 
@@ -229,15 +234,74 @@ void SetupReset()
 {
 
 
- pinMode(GREEN_LED,OUTPUT);
- pinMode(RESET_PIN,OUTPUT);
+  pinMode(GREEN_LED,OUTPUT);
+  pinMode(RESET_PIN,OUTPUT);
 
 
- ///Conenect RESET Pins to the RESET pin of launchpad,its the 16th PIN
- digitalWrite(RESET_PIN,HIGH);
+  ///Conenect RESET Pins to the RESET pin of launchpad,its the 16th PIN
+  digitalWrite(RESET_PIN,HIGH);
 
 
 }
+
+
+void Setup_MPU6050()
+{
+
+
+    Wire.begin();
+   // initialize device
+    Serial.println("Initializing I2C devices...");
+    mpu.initialize();
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(mpu.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+    //Initialize DMP in MPU 6050
+    Setup_MPU6050_DMP();
+
+
+}
+
+//Setup MPU 6050 DMP
+void Setup_MPU6050_DMP()
+{
+
+     //DMP Initialization
+
+   devStatus = mpu.dmpInitialize();
+
+   mpu.setXGyroOffset(220);
+   mpu.setXGyroOffset(76);
+   mpu.setXGyroOffset(-85);
+   mpu.setXGyroOffset(1788);
+
+
+   if(devStatus == 0)
+   {
+
+    mpu.setDMPEnabled(true);
+
+    pinMode(PUSH2,INPUT_PULLUP);
+    attachInterrupt(PUSH2, dmpDataReady, RISING);
+
+    mpuIntStatus = mpu.getIntStatus();
+
+    dmpReady = true;
+
+    packetSize = mpu.dmpGetFIFOPacketSize();
+
+   }
+   /*else{
+
+     ;
+     }*/
+
+
+}
+
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //MAIN LOOP
 
@@ -245,7 +309,7 @@ void SetupReset()
 //do_Left_Encoder() Definitions
 void do_Left_Encoder()
 {
- (digitalRead(Left_Encoder_PinB)==LOW) ? Left_Encoder_Ticks++ : Left_Encoder_Ticks--;
+  (digitalRead(Left_Encoder_PinB)==LOW) ? Left_Encoder_Ticks++ : Left_Encoder_Ticks--;
 
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -253,19 +317,19 @@ void do_Left_Encoder()
 
 void do_Right_Encoder()
 {
- (digitalRead(Right_Encoder_PinB)==LOW) ? Right_Encoder_Ticks++ : Right_Encoder_Ticks--;
+  (digitalRead(Right_Encoder_PinB)==LOW) ? Right_Encoder_Ticks++ : Right_Encoder_Ticks--;
 }
 
 //Will update both encoder value through serial port
 void Update_Encoders()
 {
 
- Serial.print("e");
- Serial.print("\t");
- Serial.print(Left_Encoder_Ticks);
- Serial.print("\t");
- Serial.print(Right_Encoder_Ticks);
- Serial.print("\n");
+  Serial.print("e");
+  Serial.print("\t");
+  Serial.print(Left_Encoder_Ticks);
+  Serial.print("\t");
+  Serial.print(Right_Encoder_Ticks);
+  Serial.print("\n");
 
 
 
@@ -273,16 +337,23 @@ void Update_Encoders()
 void loop()
 {
 
-   //Read from Serial port
-   Read_From_Serial();
-   //Send time information through serial port
-   Update_Time();
-   //Send encoders values through serial port
-   Update_Encoders();
-   //Update motor values with corresponding speed and send speed values through serial port
-   Update_Motors();
-   //Send ultrasonic values through serial port
-   //Update_Ultra_Sonic();
+    //Read from Serial port
+    Read_From_Serial();
+
+    //Send time information through serial port
+    Update_Time();
+
+    //Send encoders values through serial port
+    Update_Encoders();
+
+    //Update motor values with corresponding speed and send speed values through serial port
+    Update_Motors();
+
+    //Send ultrasonic values through serial port
+    //Update_Ultra_Sonic();
+
+    //Send MPU 6050 values through serial port
+    //Update_MPU6050();
 
 }
 
@@ -295,14 +366,14 @@ void loop()
 void Read_From_Serial()
 
 {
-  while(Serial.available() > 0)
-   {
+   while(Serial.available() > 0)
+    {
 
-      int data = Serial.read();
-      Messenger_Handler.process(data);
+       int data = Serial.read();
+       Messenger_Handler.process(data);
 
 
-   }
+    }
 
 }
 
@@ -312,25 +383,25 @@ void Read_From_Serial()
 void OnMssageCompleted()
 {
 
- char reset[] = "r";
- char set_speed[] = "s";
+  char reset[] = "r";
+  char set_speed[] = "s";
 
- if(Messenger_Handler.checkString(reset))
- {
+  if(Messenger_Handler.checkString(reset))
+  {
 
-    Serial.println("Reset Done");
-    Reset();
+     Serial.println("Reset Done");
+     Reset();
 
- }
- if(Messenger_Handler.checkString(set_speed))
- {
+  }
+  if(Messenger_Handler.checkString(set_speed))
+  {
 
-    //This will set the speed
-    Set_Speed();
-    return;
+     //This will set the speed
+     Set_Speed();
+     return;
 
 
- }
+  }
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -338,8 +409,8 @@ void OnMssageCompleted()
 void Set_Speed()
 {
 
- motor_left_speed = Messenger_Handler.readLong();
- motor_right_speed = Messenger_Handler.readLong();
+  motor_left_speed = Messenger_Handler.readLong();
+  motor_right_speed = Messenger_Handler.readLong();
 
 
 
@@ -349,10 +420,10 @@ void Set_Speed()
 void Reset()
 {
 
- digitalWrite(GREEN_LED,HIGH);
- delay(1000);
- digitalWrite(RESET_PIN,HIGH);
- digitalWrite(GREEN_LED,LOW);
+  digitalWrite(GREEN_LED,HIGH);
+  delay(1000);
+  digitalWrite(RESET_PIN,HIGH);
+  digitalWrite(GREEN_LED,LOW);
 
 
 }
@@ -362,15 +433,15 @@ void Reset()
 void Update_Motors()
 {
 
- moveRightMotor(motor_right_speed);
- moveLeftMotor(motor_left_speed);
+  moveRightMotor(motor_right_speed);
+  moveLeftMotor(motor_left_speed);
 
- Serial.print("s");
- Serial.print("\t");
- Serial.print(motor_left_speed);
- Serial.print("\t");
- Serial.print(motor_right_speed);
- Serial.print("\n");
+  Serial.print("s");
+  Serial.print("\t");
+  Serial.print(motor_left_speed);
+  Serial.print("\t");
+  Serial.print(motor_right_speed);
+  Serial.print("\n");
 
 
 }
@@ -380,23 +451,23 @@ void Update_Motors()
 
 void Update_Ultra_Sonic()
 {
- digitalWrite(Trig, LOW);
- delayMicroseconds(2);
- digitalWrite(Trig, HIGH);
- delayMicroseconds(10);
- digitalWrite(Trig, LOW);
- // The echo pin is used to read the signal from the PING))): a HIGH
- // pulse whose duration is the time (in microseconds) from the sending
- // of the ping to the reception of its echo off of an object.
- duration = pulseIn(echo, HIGH);
- // convert the time into a distance
- cm = microsecondsToCentimeters(duration);
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+  // The echo pin is used to read the signal from the PING))): a HIGH
+  // pulse whose duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  duration = pulseIn(echo, HIGH);
+  // convert the time into a distance
+  cm = microsecondsToCentimeters(duration);
 
- //Sending through serial port
- Serial.print("u");
- Serial.print("\t");
- Serial.print(cm);
- Serial.print("\n");
+  //Sending through serial port
+  Serial.print("u");
+  Serial.print("\t");
+  Serial.print(cm);
+  Serial.print("\n");
 
 }
 
@@ -406,23 +477,23 @@ void Update_Time()
 {
 
 
- CurrentMicrosecs = micros();
- LastUpdateMillisecs = millis();
- MicrosecsSinceLastUpdate = CurrentMicrosecs - LastUpdateMicrosecs;
- if (MicrosecsSinceLastUpdate < 0)
-   {
- MicrosecsSinceLastUpdate = INT_MIN - LastUpdateMicrosecs + CurrentMicrosecs;
+  CurrentMicrosecs = micros();
+  LastUpdateMillisecs = millis();
+  MicrosecsSinceLastUpdate = CurrentMicrosecs - LastUpdateMicrosecs;
+  if (MicrosecsSinceLastUpdate < 0)
+    {
+	MicrosecsSinceLastUpdate = INT_MIN - LastUpdateMicrosecs + CurrentMicrosecs;
 
-   }
- LastUpdateMicrosecs = CurrentMicrosecs;
- SecondsSinceLastUpdate = MicrosecsSinceLastUpdate / 1000000.0;
+    }
+  LastUpdateMicrosecs = CurrentMicrosecs;
+  SecondsSinceLastUpdate = MicrosecsSinceLastUpdate / 1000000.0;
 
- Serial.print("t");
- Serial.print("\t");
- Serial.print(LastUpdateMicrosecs);
- Serial.print("\t");
- Serial.print(SecondsSinceLastUpdate);
- Serial.print("\n");
+  Serial.print("t");
+  Serial.print("\t");
+  Serial.print(LastUpdateMicrosecs);
+  Serial.print("\t");
+  Serial.print(SecondsSinceLastUpdate);
+  Serial.print("\n");
 
 
 }
@@ -447,28 +518,28 @@ return microseconds / 29 / 2;
 
 void moveRightMotor(float rightServoValue)
 {
- if (rightServoValue>0)
- {
+  if (rightServoValue>0)
+  {
+    //digitalWrite(STOP_MOTOR_R,HIGH);
+    digitalWrite(A_1,HIGH);
+    analogWrite(PWM_1,rightServoValue);
+  }
+  else if(rightServoValue<0)
+  {
    //digitalWrite(STOP_MOTOR_R,HIGH);
-   digitalWrite(A_1,HIGH);
-   analogWrite(PWM_1,rightServoValue);
- }
- else if(rightServoValue<0)
- {
-  //digitalWrite(STOP_MOTOR_R,HIGH);
-  digitalWrite(A_1,LOW);
+   digitalWrite(A_1,LOW);
 
-  analogWrite(PWM_1,abs(rightServoValue));
+   analogWrite(PWM_1,abs(rightServoValue));
 
- }
+  }
 
- else if(rightServoValue == 0)
- {
-   //reset PIN , mas esse desliga o TIVA
-   //digitalWrite(STOP_MOTOR_R,LOW);
-   analogWrite(PWM_1, 0);
-   analogWrite(PWM_2, 0);
- }
+  else if(rightServoValue == 0)
+  {
+    //reset PIN , mas esse desliga o TIVA
+    //digitalWrite(STOP_MOTOR_R,LOW);
+    analogWrite(PWM_1, 0);
+    analogWrite(PWM_2, 0);
+  }
 
 
 }
@@ -476,25 +547,168 @@ void moveRightMotor(float rightServoValue)
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void moveLeftMotor(float leftServoValue)
 {
-  if (leftServoValue > 0)
- {
-   //digitalWrite(STOP_MOTOR_L,HIGH);
-   digitalWrite(B_1,HIGH);
-   analogWrite(PWM_2,leftServoValue);
- }
- else if(leftServoValue < 0)
- {
-   //digitalWrite(STOP_MOTOR_L,HIGH);
-   digitalWrite(B_1,LOW);
-   analogWrite(PWM_2,abs(leftServoValue));
- }
- else if(leftServoValue == 0)
- {
-  //reset PIN , mas esse desliga o TIVA
-    //digitalWrite(STOP_MOTOR_L,LOW);
-    analogWrite(PWM_1, 0);
-    analogWrite(PWM_2, 0);
+   if (leftServoValue > 0)
+  {
+    //digitalWrite(STOP_MOTOR_L,HIGH);
+    digitalWrite(B_1,HIGH);
+    analogWrite(PWM_2,leftServoValue);
   }
+  else if(leftServoValue < 0)
+  {
+    //digitalWrite(STOP_MOTOR_L,HIGH);
+    digitalWrite(B_1,LOW);
+    analogWrite(PWM_2,abs(leftServoValue));
+  }
+  else if(leftServoValue == 0)
+  {
+   //reset PIN , mas esse desliga o TIVA
+     //digitalWrite(STOP_MOTOR_L,LOW);
+     analogWrite(PWM_1, 0);
+     analogWrite(PWM_2, 0);
+   }
+
+
+}
+
+void Update_MPU6050()
+{
+
+
+
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+
+    ///Update values from DMP for getting rotation vector
+    Update_MPU6050_DMP();
+
+
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Update MPU6050 DMP functions
+
+void Update_MPU6050_DMP()
+{
+
+ //DMP Processing
+
+    if (!dmpReady) return;
+
+    while (!mpuInterrupt && fifoCount < packetSize){}
+
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    //get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+
+    if ((mpuIntStatus & 0x10) || fifoCount > 512)
+    {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+    }
+
+
+
+
+    else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_QUATERNION
+            // display quaternion values in easy matrix form: w x y z
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+
+
+            Serial.print("i");Serial.print("\t");
+            Serial.print(q.x); Serial.print("\t");
+            Serial.print(q.y); Serial.print("\t");
+            Serial.print(q.z); Serial.print("\t");
+            Serial.print(q.w);
+            Serial.print("\n");
+
+
+        #endif
+
+        #ifdef OUTPUT_READABLE_EULER
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
+            Serial.print("euler\t");
+            Serial.print(euler[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(euler[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(euler[2] * 180/M_PI);
+        #endif
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+        #endif
+
+        #ifdef OUTPUT_READABLE_REALACCEL
+            // display real acceleration, adjusted to remove gravity
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            Serial.print("areal\t");
+            Serial.print(aaReal.x);
+            Serial.print("\t");
+            Serial.print(aaReal.y);
+            Serial.print("\t");
+            Serial.println(aaReal.z);
+        #endif
+
+        #ifdef OUTPUT_READABLE_WORLDACCEL
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            Serial.print("aworld\t");
+            Serial.print(aaWorld.x);
+            Serial.print("\t");
+            Serial.print(aaWorld.y);
+            Serial.print("\t");
+            Serial.println(aaWorld.z);
+        #endif
+
+        #ifdef OUTPUT_TEAPOT
+            // display quaternion values in InvenSense Teapot demo format:
+            teapotPacket[2] = fifoBuffer[0];
+            teapotPacket[3] = fifoBuffer[1];
+            teapotPacket[4] = fifoBuffer[4];
+            teapotPacket[5] = fifoBuffer[5];
+            teapotPacket[6] = fifoBuffer[8];
+            teapotPacket[7] = fifoBuffer[9];
+            teapotPacket[8] = fifoBuffer[12];
+            teapotPacket[9] = fifoBuffer[13];
+            Serial.write(teapotPacket, 14);
+            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+        #endif
+
+
+    }
 
 
 }
