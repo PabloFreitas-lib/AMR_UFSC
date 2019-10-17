84b5cee8c3bd81c7e0208aa781d96922120659b0
PabloFreitasUfsc <pablo.santos1999@gmail.com>
2019-10-17 14:52:24 -0300

    removi coisas inuteis no serial e adaptei local do mapa e nav

--- pegasus_bringup/scripts/launchpad_node.py
+++ pegasus_bringup/scripts/launchpad_node.py
@@ -26,7 +26,7 @@ from sensor_msgs.msg import Imu
 
 #Class to handle serial data from Launchpad and converted to ROS topics
 class Launchpad_Class(object):
-	
+
 	def __init__(self):
 		print "Initializing Launchpad Class"
 
@@ -37,13 +37,13 @@ class Launchpad_Class(object):
 		self._left_encoder_value = 0
 		self._right_encoder_value = 0
 
-		self._battery_value = 0
-		self._ultrasonic_value = 0
+		# self._battery_value = 0
+		# self._ultrasonic_value = 0
 
-		self._qx = 0
-		self._qy = 0
-		self._qz = 0
-		self._qw = 0
+		# self._qx = 0
+		# self._qy = 0
+		# self._qz = 0
+		# self._qw = 0
 
 		self._left_wheel_speed_ = 0
 		self._right_wheel_speed_ = 0
@@ -62,62 +62,62 @@ class Launchpad_Class(object):
 		#Initializing SerialDataGateway with port, baudrate and callback function to handle serial data
 		self._SerialDataGateway = SerialDataGateway(port, baudRate,  self._HandleReceivedLine)
 		rospy.loginfo("Started serial communication")
-		
+
 
 #######################################################################################################################
 #Subscribers and Publishers
 
 		#Publisher for left and right wheel encoder values
-		self._Left_Encoder = rospy.Publisher('lwheel',Int64,queue_size = 10)		
-		self._Right_Encoder = rospy.Publisher('rwheel',Int64,queue_size = 10)		
+		self._Left_Encoder = rospy.Publisher('lwheel',Int64,queue_size = 10)
+		self._Right_Encoder = rospy.Publisher('rwheel',Int64,queue_size = 10)
 
 		#Publisher for Battery level(for upgrade purpose)
-		self._Battery_Level = rospy.Publisher('battery_level',Float32,queue_size = 10)
+		#self._Battery_Level = rospy.Publisher('battery_level',Float32,queue_size = 10)
 		#Publisher for Ultrasonic distance sensor
-		self._Ultrasonic_Value = rospy.Publisher('ultrasonic_distance',Float32,queue_size = 10)
+		# self._Ultrasonic_Value = rospy.Publisher('ultrasonic_distance',Float32,queue_size = 10)
 
 		#Publisher for IMU rotation quaternion values
-		self._qx_ = rospy.Publisher('qx',Float32,queue_size = 10)
-		self._qy_ = rospy.Publisher('qy',Float32,queue_size = 10)
-		self._qz_ = rospy.Publisher('qz',Float32,queue_size = 10)
-		self._qw_ = rospy.Publisher('qw',Float32,queue_size = 10)
-	
+		#self._qx_ = rospy.Publisher('qx',Float32,queue_size = 10)
+		#self._qy_ = rospy.Publisher('qy',Float32,queue_size = 10)
+		#self._qz_ = rospy.Publisher('qz',Float32,queue_size = 10)
+		#self._qw_ = rospy.Publisher('qw',Float32,queue_size = 10)
+
 		#Publisher for entire serial data
 		self._SerialPublisher = rospy.Publisher('serial', String,queue_size=10)
-		
+
 #######################################################################################################################
 #Subscribers and Publishers of IMU data topic
 
-		self.frame_id = '/base_link'
-
-	        self.cal_offset = 0.0
-        	self.orientation = 0.0
-        	self.cal_buffer =[]
-        	self.cal_buffer_length = 1000
-        	self.imu_data = Imu(header=rospy.Header(frame_id="base_link"))
-        	self.imu_data.orientation_covariance = [1e6, 0, 0, 0, 1e6, 0, 0, 0, 1e-6]
-	        self.imu_data.angular_velocity_covariance = [1e6, 0, 0, 0, 1e6, 0, 0, 0, 1e-6]
-        	self.imu_data.linear_acceleration_covariance = [-1,0,0,0,0,0,0,0,0]
-        	self.gyro_measurement_range = 150.0 
-        	self.gyro_scale_correction = 1.35
-        	self.imu_pub = rospy.Publisher('imu/data', Imu,queue_size = 10)
-
-		self.deltat = 0
-		self.lastUpdate = 0
+		# self.frame_id = '/base_link'
+		#
+	    #     self.cal_offset = 0.0
+        # 	self.orientation = 0.0
+        # 	self.cal_buffer =[]
+        # 	self.cal_buffer_length = 1000
+        # 	self.imu_data = Imu(header=rospy.Header(frame_id="base_link"))
+        # 	self.imu_data.orientation_covariance = [1e6, 0, 0, 0, 1e6, 0, 0, 0, 1e-6]
+	    #     self.imu_data.angular_velocity_covariance = [1e6, 0, 0, 0, 1e6, 0, 0, 0, 1e-6]
+        # 	self.imu_data.linear_acceleration_covariance = [-1,0,0,0,0,0,0,0,0]
+        # 	self.gyro_measurement_range = 150.0
+        # 	self.gyro_scale_correction = 1.35
+        # 	self.imu_pub = rospy.Publisher('imu/data', Imu,queue_size = 10)
+		#
+		# self.deltat = 0
+		# self.lastUpdate = 0
 
 #New addon for computing quaternion
-		
-		self.pi = 3.14159
-		self.GyroMeasError = float(self.pi * ( 40 / 180 ))
-		self.beta = float(math.sqrt(3 / 4) * self.GyroMeasError)
 
-		self.GyroMeasDrift = float(self.pi * ( 2 / 180 ))
-		self.zeta = float(math.sqrt(3 / 4) * self.GyroMeasDrift)
-
-
-		self.beta = math.sqrt(3 / 4) * self.GyroMeasError
-
-		self.q = [1,0,0,0]
+		# self.pi = 3.14159
+		# self.GyroMeasError = float(self.pi * ( 40 / 180 ))
+		# self.beta = float(math.sqrt(3 / 4) * self.GyroMeasError)
+		#
+		# self.GyroMeasDrift = float(self.pi * ( 2 / 180 ))
+		# self.zeta = float(math.sqrt(3 / 4) * self.GyroMeasDrift)
+		#
+		#
+		# self.beta = math.sqrt(3 / 4) * self.GyroMeasError
+		#
+		# self.q = [1,0,0,0]
 #######################################################################################################################
 #Speed subscriber
 		self._left_motor_speed = rospy.Subscriber('left_wheel_speed',Float32,self._Update_Left_Speed)
@@ -137,7 +137,7 @@ class Launchpad_Class(object):
 		self._WriteSerial(speed_message)
 
 #######################################################################################################################################################3
-				
+
 
 	def _Update_Right_Speed(self, right_speed):
 
@@ -172,71 +172,71 @@ class Launchpad_Class(object):
 					self._Left_Encoder.publish(self._left_encoder_value)
 					self._Right_Encoder.publish(self._right_encoder_value)
 
-#######################################################################################################################
-				
-				if(lineParts[0] == 'b'):
-					self._battery_value = float(lineParts[1])
-
-#######################################################################################################################
-					self._Battery_Level.publish(self._battery_value)
-
-#######################################################################################################################
-
-
-				if(lineParts[0] == 'u'):
-					self._ultrasonic_value = float(lineParts[1])
-
-
-#######################################################################################################################
-					self._Ultrasonic_Value.publish(self._ultrasonic_value)
-#######################################################################################################################
-			
-				if(lineParts[0] == 'i'):
-
-					self._qx = float(lineParts[1])
-					self._qy = float(lineParts[2])
-					self._qz = float(lineParts[3])
-					self._qw = float(lineParts[4])
-
-
-#######################################################################################################################
-					self._qx_.publish(self._qx)
-					self._qy_.publish(self._qy)
-					self._qz_.publish(self._qz)
-					self._qw_.publish(self._qw)
+# #######################################################################################################################
+#
+# 				if(lineParts[0] == 'b'):
+# 					self._battery_value = float(lineParts[1])
+#
+# #######################################################################################################################
+# 					self._Battery_Level.publish(self._battery_value)
 
 #######################################################################################################################
 
+#
+# 				if(lineParts[0] == 'u'):
+# 					self._ultrasonic_value = float(lineParts[1])
+#
+#
+# #######################################################################################################################
+# 					self._Ultrasonic_Value.publish(self._ultrasonic_value)
+#######################################################################################################################
+
+				# if(lineParts[0] == 'i'):
+#
+# 					self._qx = float(lineParts[1])
+# 					self._qy = float(lineParts[2])
+# 					self._qz = float(lineParts[3])
+# 					self._qw = float(lineParts[4])
+#
+#
+# #######################################################################################################################
+# 					self._qx_.publish(self._qx)
+# 					self._qy_.publish(self._qy)
+# 					self._qz_.publish(self._qz)
+# 					self._qw_.publish(self._qw)
+
+#######################################################################################################################
 
 
 
-					imu_msg = Imu()
-					h = Header()
-					h.stamp = rospy.Time.now()
-					h.frame_id = self.frame_id
+					#
+					# imu_msg = Imu()
+					# h = Header()
+					# h.stamp = rospy.Time.now()
+					# h.frame_id = self.frame_id
+					#
+					# imu_msg.header = h
+					#
+					# imu_msg.orientation_covariance = (-1., )*9
+					# imu_msg.angular_velocity_covariance = (-1., )*9
+					# imu_msg.linear_acceleration_covariance = (-1., )*9
+					#
+					#
+					# imu_msg.orientation.x = self._qx
+					# imu_msg.orientation.y = self._qy
+					# imu_msg.orientation.z = self._qz
+					# imu_msg.orientation.w = self._qw
+					#
+					# self.imu_pub.publish(imu_msg)
 
-					imu_msg.header = h
-
-					imu_msg.orientation_covariance = (-1., )*9	
-					imu_msg.angular_velocity_covariance = (-1., )*9
-					imu_msg.linear_acceleration_covariance = (-1., )*9
-
-
-					imu_msg.orientation.x = self._qx
-					imu_msg.orientation.y = self._qy
-					imu_msg.orientation.z = self._qz
-					imu_msg.orientation.w = self._qw
-
-					self.imu_pub.publish(imu_msg)
 
 
 
-				
 			except:
 				rospy.logwarn("Error in Sensor values")
 				rospy.logwarn(lineParts)
 				pass
-			
+
 
 
 #######################################################################################################################
@@ -258,12 +258,12 @@ class Launchpad_Class(object):
 	def Stop(self):
 		rospy.logdebug("Stopping")
 		self._SerialDataGateway.Stop()
-		
 
-		
+
+
 #######################################################################################################################
 
-	
+
 	def Subscribe_Speed(self):
 		a = 1
 #		print "Subscribe speed"
@@ -291,8 +291,8 @@ if __name__ =='__main__':
 	rospy.init_node('launchpad_ros',anonymous=True)
 	launchpad = Launchpad_Class()
 	try:
-		
-		launchpad.Start()	
+
+		launchpad.Start()
 		rospy.spin()
 	except rospy.ROSInterruptException:
 		rospy.logwarn("Error in main function")
@@ -302,5 +302,3 @@ if __name__ =='__main__':
 	launchpad.Stop()
 
 #######################################################################################################################
-
-
